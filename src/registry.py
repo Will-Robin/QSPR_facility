e9ad16383d114cecdcb9823091ccb1f769b0989d
William E. Robinson <william.robinson@ru.nl>
2026-09-14 10:15:52 +0200

    add GIN networks

--- src/registry.py
+++ src/registry.py
@@ -7,15 +7,11 @@ from src.ml_models import (
     MLPModel,
 )
 
-from src.graph_neural_networks.AttentiveFP import (
-    AttentiveFPModel,
-    AttentiveFPHeadModel,
-)
+from src.graph_neural_networks.AttentiveFP import AttentiveFPModel, AttentiveFPHeadModel
 
-from src.graph_neural_networks.GCN import (
-    GCNModel,
-    GCNHeadModel,
-)
+from src.graph_neural_networks.GCN import GCNModel, GCNHeadModel
+
+from src.graph_neural_networks.GIN import GINModel, GINHeadModel
 
 from src.dataloader import ECFPFeaturizer, DescriptorFeaturizer, GraphFeaturizer
 
@@ -30,6 +26,8 @@ MODEL_REGISTRY = {
     "attentivefp_head": AttentiveFPHeadModel,
     "gcn": GCNModel,
     "gcn_head": GCNHeadModel,
+    "gin": GINModel,
+    "gin_head": GINHeadModel,
 }
 
 
